2a9370da520d4acb1c378e14203634bdf1a5ace9
Ulises Vázquez <espinozaulises@gmail.com>
2012-09-08 19:06:46 -0500

    Version 1.2 Complete.

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -1,6 +1,6 @@
 <?php echo $basejs?>
 <?php echo $header ?>
-<div id="main" role="main" class="row">
+<div id="main" role="main" class="container">
 	<?php echo $content_body ?>
 </div>
 <?php echo $footer ?>
